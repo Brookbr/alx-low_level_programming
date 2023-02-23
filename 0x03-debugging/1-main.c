5d9e66985a3d447ebab780f93fa1941142248cf6
Brookbr <brookashebir428@gmail.com>
2023-02-23 00:03:07 -0800

    1-main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,13 +10,12 @@ int main(void)
 int i;
 
 printf("Infinite loop incoming :(\n");
-i = o;
-/*
- * while (i < 10)
- * {
- * putchar(i);
- * }
- */
+i =  o;
+
+while (i < 10)
+{
+putchar(i);
+}
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
