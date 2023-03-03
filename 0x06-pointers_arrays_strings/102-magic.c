140ac05814905bdcdcfaedac907ef448289e80ad
Brookbr <brookashebir428@gmail.com>
2023-03-03 15:20:59 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,7 +8,14 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-
+/*
+ * write your line of code
+ * Remember:
+ * - you are not allowed to modify p
+ * - You are not allowed to use the variable a in your new line of code
+ * - You can only write one statement
+ * -  You are not allowed to code anything else than the line of expected line of code at the expected line
+ */
 *(p + 5) = 98;
 /*...so that this prints 98\n */
 printf(*a[2] - % d\n *, a[2]);
