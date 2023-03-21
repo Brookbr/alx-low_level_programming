9553301bff494234319da27d92e61e719c115884
Brookbr <brookashebir428@gmail.com>
2023-03-21 09:10:14 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,7 +7,6 @@
  * @age: Second member
  * @owner: Third member
  *
- * Description: Longer description
  */
 
 struct dog
@@ -19,6 +18,7 @@ char *owner;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
